0d9835e98a2e13a06b27f158a5c378492461df36
siddhm11 <113786022+siddhm11@users.noreply.github.com>
2025-07-15 17:05:13 +0530

    hide api

--- backend/specter2_search_enhanced.py
+++ backend/specter2_search_enhanced.py
@@ -22,8 +22,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Your Qdrant Configuration
-QDRANT_URL = "https://6b25695f-de3c-4dbd-bb36-6de748ff47f2.us-east-1-0.aws.cloud.qdrant.io"
-QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.Ug0KQAaAKM7Hv-L3NprJnvuLgNcNL9D9847dfWRL_Fk"
+QDRANT_URL = "hidden"
+QDRANT_API_KEY = "ok_hidden"
 COLLECTION_NAME = "arxiv_specter2_recommendations"
 
 # ArXiv API Configuration
